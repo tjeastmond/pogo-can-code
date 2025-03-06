460abcf5044a3e84e38613a894131172dd4bf197
TJ Eastmond <tj.eastmond@gmail.com>
2025-03-06 11:40:08 -0500

    Created a config file

---
+++ src/config.ts
@@ -0,0 +1,26 @@
+// cli commands
+export const slashCommands = ["/copy", "/exit", "/files", "/review", "/test"];
+export const commands = slashCommands.map((c) => c.slice(1));
+
+export const prompts = {
+  SIMPLE_CREATE: "simple_create",
+  REVIEW: "review",
+};
+
+export const pogoIsThinking = "Pogo is thinking...";
+export const invalidCommand = "Invalid command";
+
+export const defaultLLM = "openai";
+export const defaultModels = {
+  openai: "gpt-4o-mini",
+};
+
+export default {
+  commands,
+  defaultLLM,
+  defaultModels,
+  invalidCommand,
+  pogoIsThinking,
+  prompts,
+  slashCommands,
+};
